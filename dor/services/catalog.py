b628404b353a1ccb46b1baa9d2b2375a17c82960
Roger Espinosa <roger@umich.edu>
2025-07-11 10:55:20 -0400

    shameless refactoring

--- dor/services/catalog.py
+++ dor/services/catalog.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from sqlalchemy import UUID, func, select
+from sqlalchemy import UUID, Select, func, select
 from sqlalchemy.orm import Session
 
 from dor.models.collection import Collection
@@ -12,28 +12,31 @@ def calculate_totals_query(query):
     return select(func.count()).select_from(subquery_alias)
 
 
-@dataclass
-class ObjectsManager:
-    def find(self, session: Session, object_type: str = None, start: int = 0, limit: int = 100):
-        query = select(IntellectualObject)
-        if object_type:
-            query = query.filter_by(type=object_type)
-        query = query.join(CurrentRevision)
-
+@dataclass(kw_only=True)
+class Manager:
+    def _find(self, session: Session, query: Select, start: int, limit: int):
         total_items_query = calculate_totals_query(query)
         total_items = session.execute(total_items_query).scalar_one()
 
         query = query.limit(limit).offset(start)
 
-        objects = session.execute(query).scalars()
-        page = Page(
+        items = session.execute(query).scalars()
+        return Page(
             total_items=total_items,
             offset=start,
             limit=limit,
-            items=objects
+            items=items
         )
 
-        return page
+
+@dataclass(kw_only=True)
+class ObjectsManager(Manager):
+    def find(self, session: Session, object_type: str = None, start: int = 0, limit: int = 100):
+        query = select(IntellectualObject)
+        if object_type:
+            query = query.filter_by(type=object_type)
+        query = query.join(CurrentRevision)
+        return self._find(session=session, query=query, start=start, limit=limit)
 
     def get(self, session: Session, identifier: UUID):
         query = select(IntellectualObject)
@@ -44,27 +47,13 @@ class ObjectsManager:
         return object
     
 
-@dataclass
-class CollectionsManager:
+@dataclass(kw_only=True)
+class CollectionsManager(Manager):
     def find(self, session: Session, collection_type: str = None, start: int = 0, limit: int = 100):
         query = select(Collection)
         if collection_type:
             query = query.filter_by(type=collection_type)
-
-        total_items_query = calculate_totals_query(query)
-        total_items = session.execute(total_items_query).scalar_one()
-
-        query = query.limit(limit).offset(start)
-
-        items = session.execute(query).scalars()
-        page = Page(
-            total_items=total_items,
-            offset=start,
-            limit=limit,
-            items=items
-        )
-
-        return page
+        return self._find(session=session, query=query, start=start, limit=limit)
 
     def get(self, session: Session, identifier: UUID):
         query = select(Collection)
