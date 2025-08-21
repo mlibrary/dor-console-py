4ddf42f96bb531a7caa97ced97b112c026d1495c
Samuel Sciolla <ssciolla@umich.edu>
2025-08-21 15:15:29 -0400

    Use previous and next offset from page; page to start 0 when near beginning of range; add tests for Page

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
+
 import pytest
 
-from dor.utils import Filter, FilterLabel, remove_parameter
+from dor.utils import Filter, FilterLabel, Page, remove_parameter
 
 
 @pytest.fixture
@@ -28,3 +30,59 @@ def test_filter_makes_label(parameters: dict[str, str]):
         remove_url="?alt_identifier=amjewess%3A&collection_alt_identifier=amjewess"
     )
     assert expected_label == filter.make_label(parameters)
+
+
+# Page
+
+@dataclass
+class DummyItem:
+    id: int
+    name: str
+
+
+@pytest.fixture
+def dummy_items() -> list[DummyItem]:
+    return [
+        DummyItem(id=0, name="A"),
+        DummyItem(id=1, name="B"),
+        DummyItem(id=2, name="C"),
+        DummyItem(id=3, name="D"),
+        DummyItem(id=4, name="E"),
+        DummyItem(id=5, name="F"),
+        DummyItem(id=6, name="G"),
+        DummyItem(id=7, name="H")
+    ]
+
+
+def test_page_sets_total_pages(dummy_items):
+    page = Page(total_items=len(dummy_items), offset=0, limit=2, items=dummy_items[0:2])
+
+    assert page.total_pages == 4
+
+
+def test_page_updates_offsets_in_middle_of_range(dummy_items: list[DummyItem]):
+    page = Page(total_items=len(dummy_items), offset=4, limit=2, items=dummy_items[4:6])
+
+    assert page.previous_offset == 2
+    assert page.next_offset == 6
+
+
+def test_page_updates_offsets_at_beginning_of_range(dummy_items: list[DummyItem]):
+    page = Page(total_items=len(dummy_items), offset=0, limit=2, items=dummy_items[0:2])
+
+    assert page.previous_offset == -1
+    assert page.next_offset == 2
+
+
+def test_page_updates_offsets_near_beginning_of_range(dummy_items: list[DummyItem]):
+    page = Page(total_items=len(dummy_items), offset=1, limit=2, items=dummy_items[1:3])
+
+    assert page.previous_offset == 0
+    assert page.next_offset == 3
+
+
+def test_page_updates_offsets_at_end_of_range(dummy_items: list[DummyItem]):
+    page = Page(total_items=len(dummy_items), offset=7, limit=2, items=dummy_items[7:])
+
+    assert page.previous_offset == 5
+    assert page.next_offset == -1
